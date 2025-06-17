e1acce49123805d96878d8b8811a2ebb717f1801
vasu938093@gmail.com <vasu938093@gmail.com>
2025-06-17 06:22:51 +0530

    odd and natural num

---
+++ day1.js
@@ -0,0 +1,22 @@
+
+
+// even or odd
+function even(n) {
+    if (n % 2 === 0) {
+        console.log("even number");
+    } else {
+        console.log("odd number");
+    }
+}
+
+even(5);
+
+// naturl numbers
+
+function naturl(n) {
+    for( let i=1;i<=n;i++){
+        console.log(i)
+}
+}
+naturl(10);
+
